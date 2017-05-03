0ca480d065d955829c18f0ea97593b0be9f0d180
TJHL <league@WTS14.attlocal.net>
2017-05-03 16:06:55 -0700

    Number Sorter revised

--- src/NumberSorter.java
+++ src/NumberSorter.java
@@ -6,32 +6,39 @@ public class NumberSorter {
 		String x = JOptionPane.showInputDialog("Choose a random number between 1 and 100");
 		String y = JOptionPane.showInputDialog("Choose another random number between 1 and 100");
 		String z = JOptionPane.showInputDialog("Last number between 1 and 100");
-	int a = Integer.parseInt(x);
-	int b = Integer.parseInt(y);
-	int c = Integer.parseInt(z);
-		if(a > b && a > c){
-			System.out.println(a);
-			if(b > c){
-				System.out.println(b);
+		JOptionPane.showMessageDialog(null, "I will now show them to you in ascending order...");
+		int a = Integer.parseInt(x);
+		int b = Integer.parseInt(y);
+		int c = Integer.parseInt(z);
+		if (a < b && a < c) {
+			JOptionPane.showMessageDialog(null, a);
+			if (b < c) {
+				JOptionPane.showMessageDialog(null, b);
+				JOptionPane.showMessageDialog(null, c);
+			} else {
+				JOptionPane.showMessageDialog(null, c);
+				JOptionPane.showMessageDialog(null, b);
 			}
-			else{
-				System.out.println(c);
-				System.out.println(b);
+		} else if (b < c) {
+			JOptionPane.showMessageDialog(null, b);
+			if (a < c) {
+				JOptionPane.showMessageDialog(null, a);
+				JOptionPane.showMessageDialog(null, c);
+			} else {
+				JOptionPane.showMessageDialog(null, c);
+				JOptionPane.showMessageDialog(null, a);
 			}
-		}
-		else if(b > c && b > a){
-			System.out.println(b);
-			if(a > c){
-				System.out.println(a);
+		} else {
+			JOptionPane.showMessageDialog(null, c);
+			if (b < a) {
+				JOptionPane.showMessageDialog(null, b);
+				JOptionPane.showMessageDialog(null, a);
+			} else {
+				JOptionPane.showMessageDialog(null, a);
+				JOptionPane.showMessageDialog(null, b);
 			}
-			else{
-				System.out.println(c);
-				System.out.println(b);
+
 		}
-		else if(c > a && c > b){
-			System.out.println(c);
-		}
-	
+
 	}
-	
 }
